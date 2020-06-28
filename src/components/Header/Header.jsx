dd2c808e307b6ddaf82e54a27a9e138b705a227b
Sergey Gorshkov <gorshkov88@gmail.com>
2020-06-28 21:20:26 +0400

    add header settings button logic

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,12 +1,28 @@
 import React from "react";
+import { useDispatch } from "react-redux";
+import actions from "store/actions";
+import types from "types";
 import { Caption, Wrapper, Settings, Bar } from "./Header-ui";
 
 function Header() {
+  const dispatch = useDispatch();
+
   return (
     <Wrapper>
       <Bar>
         <Caption>Header</Caption>
-        <Settings onClick={() => console.log("click")} />
+        <Settings
+          onClick={() =>
+            dispatch(
+              actions.openModal({
+                modal: {
+                  title: types.modal.MODAL_SETTINGS.title,
+                  component: types.modal.MODAL_SETTINGS.type,
+                },
+              }),
+            )
+          }
+        />
       </Bar>
     </Wrapper>
   );
