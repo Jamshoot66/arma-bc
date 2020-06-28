0eac25e51396070ff38ab903c339f5a45b3c676d
Sergey Gorshkov <gorshkov88@gmail.com>
2020-06-28 21:29:32 +0400

    clean store

--- src/store/reducers/index.js
+++ src/store/reducers/index.js
@@ -1,5 +1,4 @@
 import { combineReducers } from "redux";
-import { test } from "./test";
 import { modal } from "./modal";
 
-export default combineReducers({ test, modal });
+export default combineReducers({ modal });
