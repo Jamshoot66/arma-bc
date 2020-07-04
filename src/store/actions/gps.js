b30a11c957d549b9b99b1d22fa7cc4b2da55a280
Sergey Gorshkov <gorshkov88@gmail.com>
2020-07-04 16:43:40 +0400

    add gps redux set coords logic

--- src/store/actions/gps.js
+++ src/store/actions/gps.js
@@ -1,12 +1,41 @@
 import actionType from "store/actionTypes";
-import { parseGPS } from "utils";
+import { parseGPS, calcGPS } from "utils";
 
 export const setGPS = ({ shooter, target }) => (dispatch) => {
-  const parsedShooterCoords = parseGPS(shooter.gps);
+  const parsedShooterCoords = parseGPS(shooter?.gps);
   const parsedShooter = { ...shooter, ...parsedShooterCoords };
 
-  const parsedTargetCoords = parseGPS(target.gps);
+  const parsedTargetCoords = parseGPS(target?.gps);
   const parsedTarget = { ...target, ...parsedTargetCoords };
+
+  dispatch({
+    type: actionType.SET_COORDS,
+    payload: { shooter: parsedShooter, target: parsedTarget },
+  });
+};
+
+export const setXY = ({ shooter, target }) => (dispatch, getState) => {
+  const { shooter: storedShooter, target: storedTarget } = getState().gps;
+
+  const newShooter = {
+    x: shooter?.x ?? storedShooter.x,
+    y: shooter?.y ?? storedShooter.y,
+  };
+
+  const parsedShooterGPS = calcGPS(newShooter.x, newShooter.y);
+
+  const parsedShooter = {
+    ...newShooter,
+    ...parsedShooterGPS,
+  };
+
+  const newTarget = {
+    x: target?.x ?? storedTarget.x,
+    y: target?.y ?? storedTarget.y,
+  };
+
+  const parsedTargetGPS = calcGPS(newTarget.x, newTarget.y);
+  const parsedTarget = { ...newTarget, ...parsedTargetGPS };
 
   dispatch({
     type: actionType.SET_COORDS,
