308a5cc049b7dbbd5f5177e52c8fc6edd8f0ca9e
Sergey Gorshkov <gorshkov88@gmail.com>
2020-07-05 22:35:04 +0400

    ref distance counter

--- src/utils/math.js
+++ src/utils/math.js
@@ -17,3 +17,16 @@ export const radToDeg = (deg) => {
 export const radToMil = (deg) => {
   return (deg * 6400) / (2 * Math.PI);
 };
+
+/**
+ * @description calculate distance between two points
+ * @param {number} start.x
+ * @param {number} start.y
+ * @param {number} end.x
+ * @param {number} end.y
+ * @return {number}
+ *
+ * @usage countDistance({x:1, y:1}, {x:2, y:2}) -> ~1.41
+ */
+export const countDistance = (start, end) =>
+  Math.sqrt((start.x - end.x) ** 2 + (start.y - end.y) ** 2);
