47e5c531bcbc5cfa9d61b8f0c8be0b679b847387
Sergey Gorshkov <gorshkov88@gmail.com>
2020-07-05 22:58:15 +0400

    chore add all selectors to component

--- src/components/GPSSummary/GPSSummary.jsx
+++ src/components/GPSSummary/GPSSummary.jsx
@@ -1,15 +1,28 @@
 import React from "react";
+import PropTypes from "prop-types";
 import { Distance, Header, Direction, Height } from "./GPSSummary.ui.jsx";
 
-function GPSSummary() {
+function GPSSummary({ distance, height, direction }) {
   return (
     <React.Fragment>
       <Header />
-      <Distance value={321} />
-      <Height value={123} />
-      <Direction value={123} />
+      <Distance value={distance} />
+      <Height value={height} />
+      <Direction value={direction} />
     </React.Fragment>
   );
 }
+
+GPSSummary.propTypes = {
+  distance: PropTypes.oneOfType([PropTypes.string, PropTypes.number]),
+  height: PropTypes.oneOfType([PropTypes.string, PropTypes.number]),
+  direction: PropTypes.oneOfType([PropTypes.string, PropTypes.number]),
+};
+
+GPSSummary.defaultProps = {
+  distance: "",
+  height: "",
+  direction: "",
+};
 
 export default GPSSummary;
